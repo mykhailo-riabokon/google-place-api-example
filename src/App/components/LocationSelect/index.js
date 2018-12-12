b78282e528b15d6df4323a9288180683627033a7
mykhailo-riabokon <mykhailo.riabokon@gmail.com>
2018-12-12 20:03:22 +0200

    fetch data

--- src/App/components/LocationSelect/index.js
+++ src/App/components/LocationSelect/index.js
@@ -1,29 +1,41 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Autocomplete } from './styled.components';
 
+export const toDataSourceItems = (locations) => {
+  return locations.map((location) => ({
+    value: location.place_id, // Will be used to retrive more details
+    text: location.description,
+  }));
+};
+
 class LocationSelect extends Component {
-  onSelect = () => {};
+  state = {
+    dataSource: [],
+  };
 
   getLocations = (value) => {
-    debugger;
+    const updateDataSource = (results) => this.setState({ dataSource: toDataSourceItems(results) });
 
     if (value) {
-    }
-  };
-
-  getLocationDetails = (location) => {
-    debugger;
-  };
-
-  onChange = (value) => {
-    if (!value) {
-      console.log('cleared');
+      this.props.fetchLocations(value).then(updateDataSource);
     }
   };
 
   render() {
-    return <Autocomplete onSelect={this.getLocationDetails} onSearch={this.getLocations} onChange={this.onChange} />;
+    return (
+      <Autocomplete
+        onSelect={this.props.onLocationSelected}
+        onSearch={this.getLocations}
+        dataSource={this.state.dataSource}
+      />
+    );
   }
 }
+
+Location.propTypes = {
+  fetchLocations: PropTypes.func.isRequired,
+  onLocationSelected: PropTypes.func.isRequired,
+};
 
 export default LocationSelect;
