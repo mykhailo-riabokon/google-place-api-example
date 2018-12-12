b3a7232594613371f3a0647e18577215177895f9
mykhailo-riabokon <mykhailo.riabokon@gmail.com>
2018-12-12 20:03:12 +0200

    map results

--- src/App/index.js
+++ src/App/index.js
@@ -7,10 +7,40 @@ import Controls from './components/Controls';
 import Results from './components/Results';
 import withGooglePlaceApi from '../hocs/withGooglePlaceApi';
 
+const mapRawGoogleDetails = (rawGoogleDetails) => {
+  // More information about google address type: https://developers.google.com/maps/documentation/geocoding/intro#Types
+  const getFromAddress = (type) => {
+    const addressComponent = rawGoogleDetails.address_components.find((item) => item.types.includes(type));
+
+    return (addressComponent && addressComponent.long_name) || null;
+  };
+
+  return {
+    displayAddress: rawGoogleDetails.formatted_address,
+    streetNumber: getFromAddress('street_number'),
+    street: getFromAddress('street_address') || getFromAddress('route'),
+    postalCode: getFromAddress('postal_code'),
+    city: getFromAddress('locality'),
+    county: getFromAddress('administrative_area_level_2'),
+    state: getFromAddress('administrative_area_level_1'),
+    country: getFromAddress('country'),
+    lat: rawGoogleDetails.geometry.location.lat(),
+    lng: rawGoogleDetails.geometry.location.lng(),
+  };
+};
+
 class App extends Component {
   state = {
     googleDetails: {},
     mappedGoogleDetails: {},
+  };
+
+  updateState = (googlePlaceDetails) => {
+    this.setState({ googleDetails: googlePlaceDetails, mappedGoogleDetails: mapRawGoogleDetails(googlePlaceDetails) });
+  };
+
+  onLocationSelected = (googlePlaceId) => {
+    this.props.googlePlaceApi.getPlaceDetails(googlePlaceId).then(this.updateState);
   };
 
   render() {
@@ -22,12 +52,15 @@ class App extends Component {
           <Title>Google place API</Title>
           <Spin spinning={googlePlaceApi.isLoading}>
             <Controls onRequestConfigChanged={googlePlaceApi.reFetchGoogleApiScript} {...googlePlaceApi.config} />
-            <LocationSelect />
+            <LocationSelect
+              fetchLocations={googlePlaceApi.getPredictions}
+              onLocationSelected={this.onLocationSelected}
+            />
           </Spin>
         </ContainerColumn>
         <ContainerColumn className="bigger">
           <Results title="Google API results" data={this.state.googleDetails} />
-          <Results title="Mapped to save results" data={this.state.mappedGoogleDetails} />
+          <Results title="Parsed result" data={this.state.mappedGoogleDetails} />
         </ContainerColumn>
       </AppContainer>
     );
