d27eac4065c4df42d81f3d3aad1eeb458757ae79
mykhailo-riabokon <mykhailo.riabokon@gmail.com>
2018-12-13 21:05:21 +0200

    add cors comment

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
-const cors = require('cors')({ origin: true });
+const cors = require('cors')({ origin: true }); // Allow origins, which is only good for demo
 const apiKey = functions.config().google['api-key'];
 
 exports.getKey = functions.https.onRequest((request, response) => {
