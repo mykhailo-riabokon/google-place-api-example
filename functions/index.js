466814abdebad18881055e055474c70b769d9cdc
mykhailo-riabokon <mykhailo.riabokon@gmail.com>
2018-12-13 21:04:31 +0200

    add cors

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
 const apiKey = functions.config().google['api-key'];
 
-exports.getKey = functions.https.onRequest((_, response) => {
-  response.send({
-    key: apiKey,
+exports.getKey = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    response.send({
+      key: apiKey,
+    });
   });
 });
